306a8f9cd02c07044549f1f116624a50bcc96629
Ben Evans <ben.d.evans@gmail.com>
2021-06-07 17:08:53 +0100

    Add extra versioned tag and update versions

--- build_images.sh
+++ build_images.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-CUDA_VERSION=${1:-11.0.3}
+#CUDA_VERSION=${1:-11.0.3}
+CUDA_VERSION=${1:-11.2.2}
 CUDNN_VERSION=${2:-8}
 CUDA_BASE=nvidia/cuda:${CUDA_VERSION}-cudnn${CUDNN_VERSION}-devel-ubuntu20.04
 docker build -t cuda-base-notebook --build-arg ROOT_CONTAINER=${CUDA_BASE} https://github.com/jupyter/docker-stacks.git#master:base-notebook/
 docker build -t cuda-minimal-notebook --build-arg BASE_CONTAINER=cuda-base-notebook https://github.com/jupyter/docker-stacks.git#master:minimal-notebook/
 docker build -t cuda-scipy-notebook --build-arg BASE_CONTAINER=cuda-minimal-notebook https://github.com/jupyter/docker-stacks.git#master:scipy-notebook/
-docker build -t cuda-dl-lab --build-arg BASE_CONTAINER=cuda-scipy-notebook --build-arg CUDA_VERSION=${CUDA_VERSION} --build-arg CUDNN_VERSION=${CUDNN_VERSION} ./cuda-dl-lab/
+docker build -t cuda-dl-lab -t cuda-dl-lab:${CUDA_VERSION}-cudnn${CUDNN_VERSION} --build-arg BASE_CONTAINER=cuda-scipy-notebook --build-arg CUDA_VERSION=${CUDA_VERSION} --build-arg CUDNN_VERSION=${CUDNN_VERSION} ./cuda-dl-lab/
