d90037200779aad8be29a0858ba80afaf045ec75
Ben Evans <ben.d.evans@gmail.com>
2021-07-01 15:26:26 +0100

    Tag previous successfully built image

--- build_images.sh
+++ build_images.sh
@@ -3,6 +3,8 @@
 CUDA_VERSION=${1:-11.2.2}
 CUDNN_VERSION=${2:-8}
 CUDA_BASE=nvidia/cuda:${CUDA_VERSION}-cudnn${CUDNN_VERSION}-devel-ubuntu20.04
+# docker rmi cuda-dl-lab:previous
+docker tag cuda-dl-lab:latest cuda-dl-lab:previous
 docker build -t cuda-base-notebook --build-arg ROOT_CONTAINER=${CUDA_BASE} https://github.com/jupyter/docker-stacks.git#master:base-notebook/
 docker build -t cuda-minimal-notebook --build-arg BASE_CONTAINER=cuda-base-notebook https://github.com/jupyter/docker-stacks.git#master:minimal-notebook/
 docker build -t cuda-scipy-notebook --build-arg BASE_CONTAINER=cuda-minimal-notebook https://github.com/jupyter/docker-stacks.git#master:scipy-notebook/
