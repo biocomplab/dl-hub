7375e3516d7f255fb496bccbae7a7470939d3e39
Ben Evans <ben.d.evans@gmail.com>
2021-11-13 15:05:01 +0000

    Tidy up

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -1,26 +1,23 @@
 import os
-# from oauthenticator.github import GitHubOAuthenticator
-# from jupyterhub.auth import PAMAuthenticator
-# from jupyterhub.auth import LDAPAuthenticator
 
 
 c.JupyterHub.hub_ip = os.environ["HUB_IP"]
-#c.JupyterHub.hub_ip = '0.0.0.0'
-
 c.JupyterHub.spawner_class = "dockerspawner.DockerSpawner"
+
+# Configure Docker Spawner
 c.DockerSpawner.image = os.environ["DOCKER_JUPYTER_IMAGE"] # or 'jupyterhub/singleuser:latest'
 c.DockerSpawner.network_name = os.environ["DOCKER_NETWORK_NAME"]
 # https://jupyterhub-dockerspawner.readthedocs.io/en/latest/spawner-types.html#using-docker-swarm-not-swarm-mode
-#c.DockerSpawner.host_ip = "0.0.0.0"
+# c.DockerSpawner.host_ip = "0.0.0.0"
 c.DockerSpawner.host_ip = os.environ["DOCKER_NETWORK_NAME"]
-
-# This may need nvidia-docker2
+# NOTE: This may need nvidia-docker2
 c.DockerSpawner.extra_host_config = {'runtime': 'nvidia'}
-#c.DockerSpawner.extra_host_config = {'gpus': 'all'}
+# c.DockerSpawner.extra_host_config = {'gpus': 'all'}
+c.DockerSpawner.remove_containers = True
 
+# Set up volumes and mounts within spawned containers
 notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan/work'
 c.DockerSpawner.notebook_dir = notebook_dir
-
 # Mount the real user's Docker volume on the host to the notebook user's
 # notebook directory in the container
 c.DockerSpawner.volumes = {
@@ -32,30 +29,17 @@ c.DockerSpawner.volumes = {
     '/home/{username}': '/home/jovyan/work/data'
 }
 
-c.DockerSpawner.remove_containers = True
+# This is the simplest authenticator for setup and testing
+# c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
+# c.DummyAuthenticator.password = "<password>"
 
-c.Spawner.environment = {'GRANT_SUDO': 'yes'}
-
-# Works!
+# SSH Authenticator
 c.JupyterHub.authenticator_class = 'sshauthenticator.SSHAuthenticator'
 c.SSHAuthenticator.server_address = os.environ["AUTH_SERVER_ADDRESS"]
 c.SSHAuthenticator.server_port = 22
 
 c.Authenticator.admin_users = set(os.environ.get("ADMIN_USERS").split(" "))
-#c.Authenticator.admin_users = {'<user>'}
-#c.JupyterHub.admin_access = True  # Admins can log in as other users
-
-#c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
-#c.DummyAuthenticator.password = "<password>"
-
-#c.JupyterHub.authenticator_class = 'jupyterhub.auth.PAMAuthenticator'
-
-# c.JupyterHub.authenticator_class = GitHubOAuthenticator
-# c.GitHubOAuthenticator.oauth_callback_url = \
-#     'http://<host_ip_addr>/hub/oauth_callback'
-# c.GitHubOAuthenticator.client_id = '<client_id>'
-# c.GitHubOAuthenticator.client_secret = '<client_secret>'
-
+# c.JupyterHub.admin_access = True  # Admins can log in as other users
 
 # Configure SSL
 c.JupyterHub.ssl_key = '/srv/jupyterhub/titan.key'
@@ -64,5 +48,6 @@ c.JupyterHub.port = 443
 # Configure configurable-http-proxy to redirect http to https
 c.ConfigurableHTTPProxy.command = ['configurable-http-proxy', '--redirect-port', '80']
 
-#c.Spawner.default_url = '/lab'
+c.Spawner.environment = {'GRANT_SUDO': 'yes'}
+# c.Spawner.default_url = '/lab'
 c.Spawner.cmd=["jupyter-labhub"]
