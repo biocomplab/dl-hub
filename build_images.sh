e8f736c3e7a69ede46f7d69a7b088273af37c0c4
Ben Evans <ben.d.evans@gmail.com>
2021-12-07 13:39:50 +0000

    Add flexible argument parsing

--- build_images.sh
+++ build_images.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
-CUDA_VERSION=${1:-11.2.2}
-CUDNN_VERSION=${2:-8}
+usage() { echo "Usage: $0 [-c <x.y.z>] [-d <x>] [-n]" 1>&2; exit 1; }
+
+#CUDA_VERSION=${1:-11.2.2}
+CUDA_VERSION=11.2.2
+#CUDNN_VERSION=${2:-8}
+CUDNN_VERSION=8
+NO_CACHE=""
+
+while getopts ":c:d:n" opt; do
+	case "${opt}" in
+		c)
+			CUDA_VERSION="${OPTARG}"
+			;;
+		d)
+			CUDNN_VERSION="${OPTARG}"
+			;;
+                n)
+                        NO_CACHE=--no-cache
+                        ;;
+		*)
+			echo "Unknown option"
+			usage
+			;;
+	esac
+done
+shift $((OPTIND-1))
+
 CUDA_BASE=nvidia/cuda:${CUDA_VERSION}-cudnn${CUDNN_VERSION}-devel-ubuntu20.04
+
 # docker rmi cuda-dl-lab:previous
 # docker tag cuda-dl-lab:latest cuda-dl-lab:previous
 LAST_ID=$(docker images cuda-dl-lab:latest --format "{{.ID}}")
 docker tag $LAST_ID cuda-dl-lab:previous
-docker build -t cuda-base-notebook --build-arg ROOT_CONTAINER=${CUDA_BASE} https://github.com/jupyter/docker-stacks.git#master:base-notebook/
+docker build -t cuda-base-notebook ${NO_CACHE} --build-arg ROOT_CONTAINER=${CUDA_BASE} https://github.com/jupyter/docker-stacks.git#master:base-notebook/
 docker build -t cuda-minimal-notebook --build-arg BASE_CONTAINER=cuda-base-notebook https://github.com/jupyter/docker-stacks.git#master:minimal-notebook/
 docker build -t cuda-scipy-notebook --build-arg BASE_CONTAINER=cuda-minimal-notebook https://github.com/jupyter/docker-stacks.git#master:scipy-notebook/
 docker build -t cuda-dl-lab -t cuda-dl-lab:${CUDA_VERSION}-cudnn${CUDNN_VERSION} --build-arg BASE_CONTAINER=cuda-scipy-notebook --build-arg CUDA_VERSION=${CUDA_VERSION} --build-arg CUDNN_VERSION=${CUDNN_VERSION} ./cuda-dl-lab/
