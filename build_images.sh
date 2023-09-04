6e846e144e6a3ab7794041fd5ab9e5eddf2bdab8
mastergyp <likaifuismyson@gmail.com>
2023-09-04 16:44:39 +0800

    update docker-stacks source path and update Dockerfile

--- build_images.sh
+++ build_images.sh
@@ -38,8 +38,8 @@ CUDA_BASE=nvidia/cuda:${CUDA_VERSION}-cudnn${CUDNN_VERSION}-devel-${OS_BASE}
 # docker tag cuda-dl-lab:latest cuda-dl-lab:previous
 LAST_ID=$(docker images cuda-dl-lab:latest --format "{{.ID}}")
 docker tag $LAST_ID cuda-dl-lab:previous
-docker build -t cuda-stacks-foundation ${NO_CACHE} --build-arg ROOT_CONTAINER=${CUDA_BASE} https://github.com/jupyter/docker-stacks.git#main:docker-stacks-foundation/
-docker build -t cuda-base-notebook ${NO_CACHE} --build-arg BASE_CONTAINER=cuda-stacks-foundation https://github.com/jupyter/docker-stacks.git#main:base-notebook/
-docker build -t cuda-minimal-notebook --build-arg BASE_CONTAINER=cuda-base-notebook https://github.com/jupyter/docker-stacks.git#main:minimal-notebook/
-docker build -t cuda-scipy-notebook --build-arg BASE_CONTAINER=cuda-minimal-notebook https://github.com/jupyter/docker-stacks.git#main:scipy-notebook/
+docker build -t cuda-stacks-foundation ${NO_CACHE} --build-arg ROOT_CONTAINER=${CUDA_BASE} https://github.com/jupyter/docker-stacks.git#main:images/docker-stacks-foundation/
+docker build -t cuda-base-notebook ${NO_CACHE} --build-arg BASE_CONTAINER=cuda-stacks-foundation https://github.com/jupyter/docker-stacks.git#main:images/base-notebook/
+docker build -t cuda-minimal-notebook --build-arg BASE_CONTAINER=cuda-base-notebook https://github.com/jupyter/docker-stacks.git#main:images/minimal-notebook/
+docker build -t cuda-scipy-notebook --build-arg BASE_CONTAINER=cuda-minimal-notebook https://github.com/jupyter/docker-stacks.git#main:images/scipy-notebook/
 docker build -t cuda-dl-lab -t cuda-dl-lab:${CUDA_VERSION}-cudnn${CUDNN_VERSION} --build-arg BASE_CONTAINER=cuda-scipy-notebook --build-arg CUDA_VERSION=${CUDA_VERSION} --build-arg CUDNN_VERSION=${CUDNN_VERSION} ./cuda-dl-lab/
